3044bd498ea847de95f2040184ff95304006ba23
nasir moghtaderi <52619226+na30r@users.noreply.github.com>
2023-08-24 23:19:43 +0400

    clean up

--- movies/src/index.tsx
+++ movies/src/index.tsx
@@ -7,18 +7,19 @@ import { ReactQueryDevtools } from "react-query/devtools";
 import { BrowserRouter } from "react-router-dom";
 import { Provider as ReduxProvider } from "react-redux";
 import store from "./redux/configureStore";
+import ThemeProvider from "./Components/context/ThemeProvider";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 const queryClient = new QueryClient();
 root.render(
   // <React.StrictMode>
   <ReduxProvider store={store}>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ThemeProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
       <ReactQueryDevtools />
     </QueryClientProvider>
   </ReduxProvider>
